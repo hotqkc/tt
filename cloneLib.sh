3425e8d194d7dbc575a29ccdd52fd4627637ed3b
hotqkc <hotqkc@github.com>
2019-04-04 14:50:56 +0800

    update

--- cloneLib.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/bkaradzic/bimg.git ./bimg
-git clone https://github.com/bkaradzic/bx.git ./bx
-git clone https://github.com/bkaradzic/bgfx.git ./bgfx
-git clone https://github.com/sfml/sfml.git ./sfml
