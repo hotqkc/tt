772e0b61760ae190f4c0a6b08eee6dd1dfc2bd31
quku <quku@QMini.local>
2019-04-16 16:24:34 +0800

    update

--- xcode-ios.sh
+++ xcode-ios.sh
@@ -1,1 +1,10 @@
-cmake -E make_directory "build-ios" && cmake -E chdir "build-ios" cmake -G "Xcode" ../ -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchain/ios.toolchain.cmake -DPLATFORM=OS64COMBINED
+
+cmakeImpl() {
+	cmake -E make_directory "build-ios" && cmake -E chdir "build-ios" cmake -G "Xcode" ../ -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchain/ios.toolchain.cmake -DPLATFORM=OS64COMBINED $1
+}
+
+if [ "$1" == "test" ] ; then
+	cmakeImpl -DSXB_TEST=true
+else
+	cmakeImpl -DSXB_TEST=false
+fi
--- xcode-macos.sh
+++ xcode-macos.sh
@@ -1,1 +1,10 @@
-cmake -E make_directory "build-macos" && cmake -E chdir "build-macos" cmake -G "Xcode" ../
+
+cmakeImpl() {
+	cmake -E make_directory "build-macos" && cmake -E chdir "build-macos" cmake -G "Xcode" ../ $1
+}
+
+if [ "$1" == "test" ] ; then
+	cmakeImpl -DSXB_TEST=true
+else
+	cmakeImpl -DSXB_TEST=false
+fi
