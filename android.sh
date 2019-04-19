1527c4724885155c5ff8616a67bf73b04ff92b90
quku <quku@QMini.local>
2019-04-19 13:34:52 +0800

    update

--- android.sh
+++ android.sh
@@ -1,1 +1,5 @@
-cmake -E make_directory "build-android" && cmake -E chdir "build-android" cmake --build ../ -DCMAKE_TOOLCHAIN_FILE=../cmake/android-toolchain/android.toolchain.cmake -DANDROID_NDK=${ndk_path} -DCMAKE_BUILD_TYPE=Release -DANDROID_ABI="armeabi-v7a with NEON" 
+#cmake -E make_directory "build-android" && cmake -E chdir "build-android" cmake ../ -DCMAKE_TOOLCHAIN_FILE=../cmake/android-toolchain/android.toolchain.cmake -DANDROID_NDK=${ndk_path} -DCMAKE_BUILD_TYPE=Release -DANDROID_ABI="armeabi-v7a with NEON" ../
+
+cd build-android
+
+cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/android-toolchain/android.toolchain.cmake -DANDROID_NDK=${ndk_path} -DCMAKE_BUILD_TYPE=Release -DANDROID_ABI="armeabi-v7a with ANDROID_NDK_RELEASE" .. && cmake --build ..
